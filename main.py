c9d7a732b5c07d700377f3cf3f67d3bdef7fe041
AndrewFanT <shadowdd700@gmail.com>
2026-06-07 10:56:09 +0300

    Edit main.py and add some new strings with code
    add condition that checks the parameter and outputs the text(answer)

--- main.py
+++ main.py
@@ -1,2 +1,8 @@
-print('Hello, world')
-print('!')
+print('Hello, world!')
+
+a = print(input('(Write: Hellow or goodbye) >>> '))
+
+if a == 'hello' or 'hi':
+    print('Have a good day!')
+elif a == 'goodbye' or 'bye':
+    print('See you')
